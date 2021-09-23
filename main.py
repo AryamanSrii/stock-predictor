bf832b6fc974b6e217a9088d290b86566865b7f5
Aryaman Srivastava <aryamansrivastava2006@gmail.com>
2021-09-23 11:27:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import streamlit as st
+import numpy as np
 import pandas as pd
-import numpy as np 
-from keras.models import load_model
-import pandas_datareader as data 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import pandas_datareader as data
+from tensorflow.keras.models import load_model 
+import streamlit as st
+from datetime import date
 
-start = '2010-01-01' #10 year date starting from
-end = '2020-12-31'   #10 year date end
+
+
+start = '2010-01-01' 
+end = date.today()
 
 st.title('AmrevX Stock Predictor') #title
 user_input = st.text_input("Enter Ticker", 'AAPL') #place holder text
@@ -49,3 +52,50 @@ plt.plot(ma200)
 plt.plot(df.Close)
 st.pyplot(fig)
 
+# spliting data into training and testing
+
+data_training =pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+data_testing =pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
+
+# scaling down data in 0,1 scale
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+data_training_array= scaler.fit_transform(data_training)
+
+
+
+model = load_model('keras_model.h5')
+
+
+
+past_100_days= data_training.tail(100)
+final_df = past_100_days.append(data_testing,ignore_index = True)
+input_data =scaler.fit_transform(final_df)
+
+x_test =[]
+y_test =[]
+
+for i in range(100,input_data.shape[0]):
+    x_test.append(input_data[i-100:i])
+    y_test.append(input_data[i,0])
+
+x_test, y_test = np.array(x_test), np.array(y_test)
+y_predicted = model.predict(x_test)
+scaler = scaler.scale_
+
+scale_factor = 1/scaler[0]
+y_predicted =y_predicted*scale_factor
+y_test =y_test*scale_factor
+
+
+
+# final graph
+st.subheader('Predictions vs Orignal')
+fig2 = plt.figure(figsize=(12,6))
+plt.plot(y_test,'b',label ='Original Price')
+plt.plot(y_predicted,'r',label='Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+st.pyplot(fig2)
